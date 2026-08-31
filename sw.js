71281567dafed2d385b514368de1a0c73e992e3a
Amey <ameybonde2006@gmail.com>
2026-09-01 02:03:34 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,38 @@
-const CACHE="budget-pwa-v2";const ASSETS=["./","./index.html","./app.js","./manifest.json","./icon.svg"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{let c=x.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return x}).catch(()=>caches.match("./index.html")))));
+const CACHE = "budget-pwa-v7";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.json",
+  "./icon.svg",
+  "./csv-templates/transactions_template.csv",
+  "./csv-templates/accounts_template.csv",
+  "./csv-templates/investments_template.csv",
+  "./csv-templates/autopay_template.csv"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((k) => {
+          if (k !== CACHE) return caches.delete(k);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request).catch(() => caches.match("./index.html")))
+  );
+});
